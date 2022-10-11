e4844d1c0fc1e0fe9e5e4298ae4c06bfbbe38576
PabloConstantinoUCU <PABLO.CONSTANTINO@correo.ucu.edu.uy>
2022-10-10 23:48:23 -0300

    Program.cs arreglado, agregadas PublishToTwitter

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -10,7 +10,6 @@ namespace PII_Herencia
         {
             
             var twitter = new TwitterImage();
-            Console.WriteLine(twitter.PublishToTwitter("New Employee 2! ","../../Gol.jpg"));
             /*
             En éste método deberas mostrar un ejemplo de funcionamiento de tu programa an pseudocódigo. A continuación te 
             planteamos un ejemplo de como hacerlo. Esto no significa que te limites a hacer solamente esto, debes pensar 
@@ -19,10 +18,15 @@ namespace PII_Herencia
             Vehiculo Kicks1 = new Vehiculo("Nissan", "Kicks");
 
             Person pasajero1 = new Pasajero("Lucas", "Peguri", "99999999");
+            Console.WriteLine(twitter.PublishToTwitter("Nuevo pasajero: Lucas Peguri","../../Peguri.jpg"));
             Person pasajero2 = new Pasajero("Ignacio", "Berra", "12345678");
+            Console.WriteLine(twitter.PublishToTwitter("Nuevo pasajero: Ignacio Berra","../../Berra.jpg"));
             Person pasajero3 = new Pasajero("Vinta", "Grios", "11111111");
+            Console.WriteLine(twitter.PublishToTwitter("Nuevo pasajero: Vinta Grios","../../Vintagrios.jpg"));
             Person conductorComun1 = new Conductor("Enzo", "Cantoni", "54321840", VolkswagenUp1, 2);
+            Console.WriteLine(twitter.PublishToTwitter("Nuevo Conductor: Enzo Cantoni","../../Cantoni.jpg"));
             Person conductorPool1 = new Conductor("Matias", "La Cruz", "5363561", Kicks1, 4);
+            Console.WriteLine(twitter.PublishToTwitter("Nuevo Conductor: Matias La Cruz","../../LaCruz.jpg"));
 
            /*  UcuRideShare rideShare = new UcuRideShare();
             
